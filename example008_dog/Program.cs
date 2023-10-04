facb6e5a2b76c9c05d493bbe13f3c2b863a24a33
Veselkova Irina <veselkovaiv@yandex.ru>
2023-10-04 18:42:29 +0300

    project with dog

---
+++ example008_dog/Program.cs
@@ -0,0 +1,23 @@
+﻿int count = 0;
+int distance = 10000;
+int FirstFriendSpeed = 2;
+int SecondFriendSpeed = 3;
+int dogSpeed = 5;
+bool friend2 = true;
+int time = 0;
+
+{
+    while (distance > 10)
+    {
+        if (friend2 == false)
+        {
+            time = distance / (FirstFriendSpeed + dogSpeed);
+            friend2 = true;
+        }
+        time = distance / (SecondFriendSpeed + dogSpeed);
+        friend2 = true;
+    distance = distance - (FirstFriendSpeed + SecondFriendSpeed) * time;
+    count++;
+    }
+    Console.WriteLine(count);
+} 
